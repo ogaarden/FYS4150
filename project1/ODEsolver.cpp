60591b297f9ae8714c7811c88aab05d16fe4d75e
ogaarden <gardenolemorten@gmail.com>
2026-08-29 22:33:40 +0200

    implementert error analyse

--- project1/ODEsolver.cpp
+++ project1/ODEsolver.cpp
@@ -3,49 +3,36 @@
 #include <vector>
 #include <fstream>
 #include <armadillo>
-
-std::vector<double> analyticalSolver(const std::vector<double>& X){
-
-    int N = X.size();
-
-    std::vector<double> u(N, 0);
-
-
-    for(int i = 0; i < N; i++){
-        u[i] = 1 - 1* (1 - exp(-10)) * X[i] - exp(-10*X[i]);
-    }
-
-    return u;
-}
-
-
-// u(n) = (u(n+1) - 2 u(n) - u(n-1)) / (2 * dx)
-// 
+#include <iomanip>
 
 double f(double x){
     return 100 * exp(-10*x);
 }
 
-std::vector<double> numericalSolution(const std::vector<double>& X, const float & dt){
-    int N = X.size();
-
-    std::vector<double> u(N, 0);
-    u[0] = 0;
-    u[u.size()-1] = 0;
-    float dx = X[1] - X[0];
-    // 2*dt f_i + 2 u(n) - u(n-1) = u(n+1)
-    for(int i = 1; i < N; i++){
-        u[i+1] = 2*u[i]  - u[i-1] -dx*dx*f(X[i]);
-    }
-
-    return u;
+double u(double x){
+    return 1 - 1* (1 - exp(-10)) * x - exp(-10*x);
 }
 
-void saveToFile(const std::vector<double>& X, const std::vector<double>& u, const std::string& filename){
+std::vector<double> analyticalSolver(const std::vector<double>& X){
 
-    std::ofstream file(filename);
+    int N = X.size();
 
-    file << "X,u\n";
+    std::vector<double> u_(N, 0);
+
+
+    for(int i = 0; i < N; i++){
+        u_[i] = u(X[i]);
+    }
+    return u_;
+}
+
+void saveToFile(const std::vector<double>& X, const std::vector<double>& u, const std::string& path){
+
+    std::ofstream file(path);
+
+    file << std::fixed << std::setprecision(3);
+
+    file << "X,u," << X.size() << "\n";
 
     for (int i = 0; i < X.size(); i++){
         file << X[i] << "," << u[i] << "\n";
@@ -54,37 +41,101 @@ void saveToFile(const std::vector<double>& X, const std::vector<double>& u, cons
     file.close();
 }
 
-arma::mat matrix(const std::vector<double>& X){
+std::vector<double> discretizedf(const std::vector<double>& X){
+    std::vector<double>fd (X.size());
+    double h = X[1] - X[0];
+    for(int i = 0; i < X.size(); i++){
+        fd[i] = f(X[i])*h*h;
+    }
+
+    return fd;
+}
+
+arma::mat tridiagonalMatrix(const std::vector<double>& X, double& diag, double& offdiag){
     int n = X.size();
     arma::mat A = arma::mat(n,n).fill(0.);
     
-    A.diag(0).fill(-2.0);
-    A.diag(-1).fill(1.0);
-    A.diag(1).fill(1.0);
-
+    A.diag(0).fill(diag);
+    A.diag(-1).fill(offdiag);
+    A.diag(1).fill(offdiag);
 
     return A;
 }
 
-int main(){
+std::vector<double> thomasAlgorithm(
+    const std::vector<double>& X,
+    std::vector<double> fd,
+    arma::mat& A
+){
+    int n = X.size();
+    std::vector<double> v(n);
 
-    int N = 1000;
-    int T = 1;
-    std::vector<double> X(N, 0);
-
-    double dt = static_cast<double>(T) / N;
-
-    for(int i = 0; i < N+1; i++){
-        X[i] = i*dt;
+    for(int i = 1; i < n; i++){
+        double m = A(i, i-1) / A(i-1, i-1);
+        A(i,i) -= m * A(i-1,i);
+        fd[i] -= m * fd[i-1];
     }
 
-    std::vector<double>u_analytic = analyticalSolver(X);
-    std::vector<double>u_numerical = numericalSolution(X, dt);
+    v[n-1] = fd[n-1] / A(n-1,n-1);
+
+    for (int i = n-2; i>=0; i--){
+        v[i] = fd[i] - (A(i,i+1) * v[i+1] / A(i,i));
+    }
+
+    return v;
+}
+
+void relativeError(
+    const std::vector<double>& X,
+    const std::vector<double> & u_numerical,
+    const std::string& path
+){
+    int n = X.size();
+
+    double s = 0;
+
+    for(int i = 1; i < X.size()-1; i++){
+        s += log10(abs(u(X[i]) - u_numerical[i]));
+    }
+
+    saveToFile(X, u_numerical, path);
+}
 
 
-    saveToFile(X, u_analytic, "data_exact.csv");
-    saveToFile(X, u_numerical, "data_numerical.csv");
+int main(){
+
+    std::vector<int> N({10, 100, 1000});
+    double L = 1.0;
+
+    double diag = -2;
+    double offdiag = 1;
+
+    std::vector<double>X_analytical;
+
+    for(int i = 0; i < N.size(); i++){
+        std::vector<double> X(N[i],0);
+        double dx = L/N[i];
+
+        for (int j = 0; j < N[i]; j++){
+            X[j] = j*dx;
+        }
+
+        arma::mat A = tridiagonalMatrix(X, diag, offdiag);
+        std::vector<double> fd = discretizedf(X);
+        std::vector<double>u_numerical = thomasAlgorithm(X, fd, A);
+
+        //saveToFile(X, u_numerical, "data/data_numerical" + std::to_string(N[i]) + ".csv");
+        relativeError(X, u_numerical, "data/rel_error" + std::to_string(N[i]) + ".csv");
+
+        if (i == N.size()-1){
+            X_analytical = X;
+        }
+    }
+
+    //std::vector<double>u_analytic = analyticalSolver(X_analytical);
+    //saveToFile(X_analytical, u_analytic, "data_exact.csv");
 
 
     return 0;
 }
+
